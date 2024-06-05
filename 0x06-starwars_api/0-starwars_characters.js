f126bae1b285b9b325684c9b1f8e90574929449e
RahwaLegesse31 <rahwalegesse31@gmail.com>
2024-06-05 06:12:39 -0700

    ggg

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
+// Import the 'request' library
 const request = require('request');
 
+// Define constant with the base URL of the Star Wars API
 const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-// Check if the number of
+// Check if the number of command line arguments is greater than 2
 if (process.argv.length > 2) {
-  // Make a request 
+  // Make a request to the film resource for the specified film ID
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     // If an error occurred during the request, log the error
     if (err) {
@@ -29,7 +31,8 @@ if (process.argv.length > 2) {
         });
       }));
 
-    // Wait Promise.all(charactersName)
+    // Wait for all Promises to resolve and log the names of the characters, separated by new lines
+    Promise.all(charactersName)
       .then(names => console.log(names.join('\n')))
       .catch(allErr => console.log(allErr));
   });
